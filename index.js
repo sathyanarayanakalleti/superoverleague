c569ade4a7bd0dfd8df6691d2db904abbb00775b
sathyanarayanakalleti <sathyanarayanakalleti@gmail.com>
2021-12-10 06:30:16 +0000

    super over league

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+  <div className="supertext">
+    <div>
+      <h1 className="stext">Super Over League</h1>
+      <img
+        className="img1"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="img1"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
